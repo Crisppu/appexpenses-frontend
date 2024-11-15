a2be8f6ebf44a6ebd42dbfc3f866bb382bf97f47
crisppu <cristobalyat69@hotmail.com>
2024-11-15 12:42:44 -0600

    initial project structure

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  apiUrl: 'http://localhost:3000/',
+  apiUrl2: "http://localhost:3000/",
+  authUrl: 'http://localhost:3000/auth',
+  featureFlag: false
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environmentLocal = {
+  production: false,
+  apiUrl: 'http://localhost:3000/',
+  featureFlag: true
+};
