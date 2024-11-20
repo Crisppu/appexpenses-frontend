536666cd4a5cb8615e5b4c473677f9eeb3d0dfbf
crisppu <cristobalyat69@hotmail.com>
2024-11-20 12:35:00 -0600

    Add navigation bar(navbar) component

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../../shared/components/navbar/navbar.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    NavbarComponent
+],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
